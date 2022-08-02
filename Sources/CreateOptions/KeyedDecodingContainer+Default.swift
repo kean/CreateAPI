36bfb33759e21af5384b1a687338c6553643cb73
Liam Nichols <liam.nichols.ln@gmail.com>
2022-08-02 14:31:52 +0200

    Add StringCodingContainer wrapper to helo record issues found in containers into IssueRecorder

--- Sources/CreateOptions/KeyedDecodingContainer+Default.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension KeyedDecodingContainer {
-    func decode<T: Decodable>(_ type: T.Type, forKey key: K, defaultValue: T) throws -> T {
-        try decodeIfPresent(type, forKey: key) ?? defaultValue
-    }
-}
