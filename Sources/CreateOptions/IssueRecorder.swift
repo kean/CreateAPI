551e4f1dcafa1711bacabc2d8ea41b82d1162d5d
Liam Nichols <liam.nichols.ln@gmail.com>
2022-08-02 14:30:59 +0200

    Add IssueRecorder concept for recording problems found in configuration files

---
+++ Sources/CreateOptions/IssueRecorder.swift
@@ -0,0 +1,39 @@
+import Foundation
+
+/// A utility used for recording deserialisation issues and warnings
+class IssueRecorder {
+    struct Issue {
+        let type: IssueType
+        let key: CodingKey
+        let path: [CodingKey]
+        let message: String
+    }
+
+    enum IssueType {
+        case unexpected, deprecated
+    }
+
+    private(set) var issues: [Issue] = []
+
+    func record(_ type: IssueType, key: CodingKey, path: [CodingKey], message: String = "") {
+        issues.append(Issue(type: type, key: key, path: path, message: message))
+    }
+}
+
+extension IssueRecorder.Issue: CustomStringConvertible {
+    var propertyName: String {
+        let completePath = path + [key]
+        return completePath
+            .map(\.stringValue)
+            .joined(separator: ".")
+    }
+
+    var description: String {
+        switch type {
+        case .deprecated:
+            return "The property '\(propertyName)' has been deprecated"
+        case .unexpected:
+            return "Found an unexpected property '\(propertyName)'"
+        }
+    }
+}
