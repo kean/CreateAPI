ae62318f307f85f4f5e125095195c46ab605cd40
Ethan Pippin <ethanpippin2343@gmail.com>
2022-08-05 22:53:01 -0600

    update tests

---
+++ Tests/CreateAPITests/Expected/petstore-internal-access-control/Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "petstore-internal-access-control",
+    platforms: [.iOS(.v13), .macCatalyst(.v13), .macOS(.v10_15), .watchOS(.v6), .tvOS(.v13)],
+    products: [
+        .library(name: "petstore-internal-access-control", targets: ["petstore-internal-access-control"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kean/Get", from: "1.0.2"), 
+        .package(url: "https://github.com/CreateAPI/HTTPHeaders", from: "0.1.0"), 
+        .package(url: "https://github.com/CreateAPI/URLQueryEncoder", from: "0.2.0")
+    ],
+    targets: [
+        .target(name: "petstore-internal-access-control", dependencies: [
+            .product(name: "Get", package: "Get"), 
+            .product(name: "HTTPHeaders", package: "HTTPHeaders"), 
+            .product(name: "URLQueryEncoder", package: "URLQueryEncoder")
+        ], path: "Sources")
+    ]
+)
